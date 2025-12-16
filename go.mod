0895d2e39036310583013e4ecab1d067c0894069
dtsiomapdx <dev.dtsioma@gmail.com>
2025-12-16 13:17:39 -0800

    post tweet

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/daniiltsioma/twitter
+
+go 1.25.1
+
+require (
+	github.com/go-chi/chi v1.5.5
+	gorm.io/driver/sqlite v1.6.0
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gorm.io/gorm v1.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Tweet struct {
+	ID int64 	`json:"id"`
+	Text string `json:"text"`
+}
+
+func main() {
+	var err error
+	db, err = gorm.Open(sqlite.Open("twitter.db"))
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	db.AutoMigrate(&Tweet{})
+
+	r := chi.NewRouter()
+
+	r.Post("/tweet", postTweet)
+
+	fmt.Printf("server listening on port 8080\n")
+	http.ListenAndServe(":8080", r)
+}
+
+func postTweet(w http.ResponseWriter, r *http.Request) {
+	var tweet Tweet
+	if err := json.NewDecoder(r.Body).Decode(&tweet); err != nil {
+		http.Error(w, "invalid JSON: " + err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if tweet.Text == "" {
+		http.Error(w, "tweet text cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	if err := gorm.G[Tweet](db, gorm.WithResult()).Create(r.Context(), &tweet); err != nil {
+		http.Error(w, "could not save tweet: " + err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(tweet)
+}
