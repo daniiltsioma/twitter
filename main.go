f45ab30fac45de6498f36fc61f34844e8f5b9bc8
dtsiomapdx <dev.dtsioma@gmail.com>
2025-12-16 13:59:35 -0800

    follow/unfollow

--- main.go
+++ main.go
@@ -14,14 +14,22 @@ import (
 var db *gorm.DB
 
 type Tweet struct {
-	ID int64 	`json:"id"`
+	ID int64 	`json:"id" gorm:"primaryKey"`
 	Text string `json:"text"`
 }
 
 type User struct {
-	ID int64 `json:"id"`
+	ID int64 `json:"id" gorm:"primaryKey"`
 	Username string `json:"username" gorm:"uniqueIndex"`
 } 
+
+type Follow struct {
+	ID int64
+	UserId int64 `json:"userId" gorm:"primaryKey,foreignKey:User"`
+	User User `gorm:"foreignKey:UserId"`
+	TargetUserId int64 `json:"targetUserId" gorm:"primaryKey"`
+	TargetUser User `gorm:"foreignKey:TargetUserId"`
+}
 
 func main() {
 	var err error
@@ -30,7 +38,7 @@ func main() {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
 
-	db.AutoMigrate(&Tweet{}, &User{})
+	db.AutoMigrate(&Tweet{}, &User{}, &Follow{})
 
 	r := chi.NewRouter()
 
@@ -39,6 +47,9 @@ func main() {
 
 	r.Post("/user", createUser)
 	r.Get("/user/{username}", getUser)
+
+	r.Post("/follow", followUser)
+	r.Delete("/follow", unfollowUser)
 
 	fmt.Printf("server listening on port 8080\n")
 	http.ListenAndServe(":8080", r)
@@ -109,4 +120,62 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(user)
+}
+
+func followUser(w http.ResponseWriter, r *http.Request) {
+	var follow Follow
+
+	if err := json.NewDecoder(r.Body).Decode(&follow); err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	if follow.UserId == 0 || follow.TargetUserId == 0 {
+		http.Error(w, "missing fields", http.StatusBadRequest)
+		return
+	}
+
+	if follow.UserId == follow.TargetUserId {
+		http.Error(w, "userId cannot be the same as targetUserId", http.StatusBadRequest)
+		return
+	}
+
+	if err := gorm.G[Follow](db, gorm.WithResult()).Create(r.Context(), &follow); err != nil {
+		http.Error(w, "could not follow user: " + err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(follow)
+}
+
+func unfollowUser(w http.ResponseWriter, r *http.Request) {
+	var unfollow Follow
+	if err := json.NewDecoder(r.Body).Decode(&unfollow); err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	if unfollow.UserId == 0 || unfollow.TargetUserId == 0 {
+		http.Error(w, "missing fields", http.StatusBadRequest)
+		return
+	}
+
+	if unfollow.UserId == unfollow.TargetUserId {
+		http.Error(w, "userId cannot be the same as targetUserId", http.StatusBadRequest)
+		return
+	}
+
+	n, err := gorm.G[Follow](db).Where("user_id = ? AND target_user_id = ?", unfollow.UserId, unfollow.TargetUserId).Delete(r.Context())
+	if err != nil {
+		http.Error(w, "could not unfollow user: " + err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if n == 0 {
+		http.Error(w, "no follow record found", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"unfollow": "success"})
 }
