6c967bf2b38e4e4ca69544df4f7a4c34060c0c3c
dtsiomapdx <dev.dtsioma@gmail.com>
2025-12-17 11:31:54 -0800

    timeline

--- main.go
+++ main.go
@@ -14,26 +14,29 @@ import (
 var db *gorm.DB
 
 type Tweet struct {
-	ID int64 	`json:"id" gorm:"primaryKey"`
+	ID int64 `gorm:"primaryKey"`
+	UserID int64 `json:"userId"`
+	User User `gorm:"foreignKey:UserID"`
 	Text string `json:"text"`
 }
 
 type User struct {
-	ID int64 `json:"id" gorm:"primaryKey"`
+	ID int64 `gorm:"primaryKey"`
 	Username string `json:"username" gorm:"uniqueIndex"`
+	Follows []Follow `gorm:"foreignKey:FollowerID"`
 } 
 
 type Follow struct {
-	ID int64
-	UserId int64 `json:"userId" gorm:"primaryKey,foreignKey:User"`
-	User User `gorm:"foreignKey:UserId"`
-	TargetUserId int64 `json:"targetUserId" gorm:"primaryKey"`
-	TargetUser User `gorm:"foreignKey:TargetUserId"`
+	ID int64 `gorm:"primaryKey"`
+	FollowerID int64 `json:"followerId"`
+	FollowedID int64 `json:"followedId"`
+	Follower User `gorm:"foreignKey:FollowerID"`
+	Followed User `gorm:"foreignKey:FollowedID"`
 }
 
 func main() {
 	var err error
-	db, err = gorm.Open(sqlite.Open("twitter.db"))
+	db, err = gorm.Open(sqlite.Open("twitter.db?_foreign_keys=on"), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
@@ -51,6 +54,8 @@ func main() {
 	r.Post("/follow", followUser)
 	r.Delete("/follow", unfollowUser)
 
+	r.Get("/timeline/{userID}", getUserTimeline)
+
 	fmt.Printf("server listening on port 8080\n")
 	http.ListenAndServe(":8080", r)
 }
@@ -62,8 +67,13 @@ func postTweet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if tweet.UserID == 0 {
+		http.Error(w, "tweet.UserID required", http.StatusBadRequest)
+		return
+	}
+
 	if tweet.Text == "" {
-		http.Error(w, "tweet text cannot be empty", http.StatusBadRequest)
+		http.Error(w, "tweet.Text cannot be empty", http.StatusBadRequest)
 		return
 	}
 
@@ -97,7 +107,7 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if user.Username == "" {
-		http.Error(w, "username cannot be empty", http.StatusBadRequest)
+		http.Error(w, "user.Username cannot be empty", http.StatusBadRequest)
 		return
 	}
 
@@ -130,12 +140,12 @@ func followUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if follow.UserId == 0 || follow.TargetUserId == 0 {
+	if follow.FollowerID == 0 || follow.FollowedID == 0 {
 		http.Error(w, "missing fields", http.StatusBadRequest)
 		return
 	}
 
-	if follow.UserId == follow.TargetUserId {
+	if follow.FollowerID == follow.FollowedID {
 		http.Error(w, "userId cannot be the same as targetUserId", http.StatusBadRequest)
 		return
 	}
@@ -156,17 +166,17 @@ func unfollowUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if unfollow.UserId == 0 || unfollow.TargetUserId == 0 {
+	if unfollow.FollowerID == 0 || unfollow.FollowedID == 0 {
 		http.Error(w, "missing fields", http.StatusBadRequest)
 		return
 	}
 
-	if unfollow.UserId == unfollow.TargetUserId {
+	if unfollow.FollowerID == unfollow.FollowedID {
 		http.Error(w, "userId cannot be the same as targetUserId", http.StatusBadRequest)
 		return
 	}
 
-	n, err := gorm.G[Follow](db).Where("user_id = ? AND target_user_id = ?", unfollow.UserId, unfollow.TargetUserId).Delete(r.Context())
+	n, err := gorm.G[Follow](db).Where("follower_id = ? AND followed_id = ?", unfollow.FollowerID, unfollow.FollowedID).Delete(r.Context())
 	if err != nil {
 		http.Error(w, "could not unfollow user: " + err.Error(), http.StatusInternalServerError)
 		return
@@ -178,4 +188,15 @@ func unfollowUser(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"unfollow": "success"})
+}
+
+func getUserTimeline(w http.ResponseWriter, r *http.Request) {
+	userID := chi.URLParam(r, "userID")
+
+	var tweets []Tweet
+
+	db.Joins("JOIN follows on follows.followed_id = tweets.user_id").Where("follows.follower_id = ?", userID).Find(&tweets)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(tweets)
 }
